bb22dab6e330e4304a7ca6abe1236652288cd788
rabelais88 <sungryeolp@gmail.com>
2019-04-22 18:39:09 +0900

    added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  distDir: 'build'
-});
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+
+const defaultConfig = {
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html'
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html'
+    }
+  },
+};
+if (process.env.NODE_ENV === 'production') {
+  defaultConfig.distDir = 'build';
+}
+
+module.exports = withBundleAnalyzer(withSass({
+  ...defaultConfig,
+  webpack(_config, options) {
+    return _config;
+  }
+}));
--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ Home.getInitialProps = async props => {
   // const res = await fetch('https://api.github.com/repos/zeit/next.js');
   // const json = await res.json();
   // return { stars: json.stargazers_count };
-  console.log(props);
+  // console.log(props);
   return [];
 };
 
