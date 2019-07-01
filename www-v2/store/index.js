5dfc9b765914e1e5cd4053d36a7e48a387fcf3ea
rabelais88 <sungryeolp@gmail.com>
2019-07-01 16:35:30 +0900

    redux persist applied

--- www-v2/store/index.js
+++ www-v2/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
+
 import { enhanceAll } from '../lib/util';
 import { countInitialState, countReducer } from './count';
 
