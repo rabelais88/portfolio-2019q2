28075349964f73b65dad8e8136bb56e1b9ece6ed
rabelais88 <sungryeolp@gmail.com>
2019-07-01 16:36:30 +0900

    redux persist applied

--- www-v2/store/index.js
+++ www-v2/store/index.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { enhanceAll } from '../lib/util';
 import { countInitialState, countReducer } from './count';
@@ -13,9 +15,16 @@ const reducers = combineReducers({
   count: countReducer,
 });
 
+const persistConfig = {
+  key: 'primary',
+  storage,
+  whitelist: ['count'], // place to select which state you want to persist
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const initializeStore = (initialState = combinedInitialState) =>
   createStore(
-    reducers,
+    persistedReducer,
     initialState,
     enhanceAll(thunkMiddleware, [applyMiddleware, composeWithDevTools]),
   );
