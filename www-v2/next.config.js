7716e39fe002725f71ccde8176af7f0fbdd7f975
rabelais88 <sungryeolp@gmail.com>
2019-07-04 11:24:41 +0900

    added bundle analyzer

--- www-v2/next.config.js
+++ www-v2/next.config.js
@@ -1,13 +1,27 @@
 const withCSS = require('@zeit/next-css');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 const { enhanceAll } = require('./lib/util');
 
 const webpackConfig = {
   plugins: {
     // 'postcss-css-variables': {},
   },
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html',
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html',
+    },
+  },
+  webpack: config => config,
 };
 
-const enhancers = [withCSS];
+const enhancers = [withCSS, withBundleAnalyzer];
 const enhanced = enhanceAll(webpackConfig, enhancers);
 
 module.exports = enhanced;
