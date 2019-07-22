a0b25fcf74ceac4ae60ee861cdc1390d209dbafe
sungryeol <sungryeolp@gmail.com>
2019-07-23 02:49:46 +0900

    added proptypes

--- www-v2/components/PostBadge.js
+++ www-v2/components/PostBadge.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const PostBadge = props => {
   const { postTitle, updated } = props;
-  return <div>post badge - {postTitle} / {JSON.stringify(updated)}</div>;
+  return (
+    <div>
+      post badge - {postTitle} / {JSON.stringify(updated)}
+    </div>
+  );
+};
+
+PostBadge.defaultProps = {
+  postTitle: '...',
+  updated: new Date(),
+};
+
+PostBadge.propTypes = {
+  postTitle: PropTypes.string,
+  updated: PropTypes.instanceOf(Date),
 };
 
 export default PostBadge;
