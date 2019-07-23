d21ae43d9a10b0e654d026467b9ff7d8af1220b4
rabelais88 <sungryeolp@gmail.com>
2019-07-23 17:21:28 +0900

    [admin]fixed vuex structure

--- admin-vue/src/api/editing.js
+++ admin-vue/src/api/editing.js
@@ -71,3 +71,39 @@ export const modifyStack = stack =>
 
 export const deleteStack = stackId =>
   request({ url: `info/stack/${stackId}`, method: 'delete' });
+
+export const getWorks = queries => {
+  const query = Object.entries(queries)
+    .map(q => `${q[0]}=${q[1]}`)
+    .join('&');
+  return request({
+    url: `/info/works?${query}`,
+    method: 'get',
+  });
+};
+
+export const createWork = newWork =>
+  request({
+    url: '/info/work',
+    method: 'work',
+    data: newWork,
+  });
+
+export const getWork = workId =>
+  request({
+    url: `/info/work/${workId}`,
+    method: 'get',
+  });
+
+export const modifyWork = work =>
+  request({
+    url: `info/work`,
+    method: 'patch',
+    data: work,
+  });
+
+export const deleteWork = workId =>
+  request({
+    url: `info/work/${workId}`,
+    method: 'delete',
+  });
--- admin-vue/src/store/modules/editing.js
+++ admin-vue/src/store/modules/editing.js
@@ -13,20 +13,29 @@ import {
 } from '@/api/editing';
 import { Notification } from 'element-ui';
 import Vue from 'vue';
-const SET_POST = 'SET_POST';
-const SET_INTRO = 'SET_INTRO';
-const SET_POSTS = 'SET_POSTS';
-const ADD_POST = 'ADD_POST';
-const SET_TOTAL_PAGES = 'SET_TOTAL_PAGES';
-const SET_POST_PAGE = 'SET_POST_PAGE';
-const SET_SORT = 'SET_SORT';
-const SET_POST_SEARCH = 'SET_POST_SEARCH';
-const SET_STACKS = 'SET_STACKS';
-const SET_STACK = 'SET_STACK';
-const SET_NEW_STACK = 'SET_NEW_STACK';
-const SET_EDITING_STACK = 'SET_EDITING_STACK';
+
+const EDITING_MUTATIONS = {
+  SET_POST: 'SET_POST',
+  SET_INTRO: 'SET_INTRO',
+  SET_POSTS: 'SET_POSTS',
+  ADD_POST: 'ADD_POST',
+  SET_TOTAL_PAGES: 'SET_TOTAL_PAGES',
+  SET_POST_PAGE: 'SET_POST_PAGE',
+  SET_SORT: 'SET_SORT',
+  SET_POST_SEARCH: 'SET_POST_SEARCH',
+  SET_STACKS: 'SET_STACKS',
+  SET_STACK: 'SET_STACK',
+  SET_NEW_STACK: 'SET_NEW_STACK',
+  SET_EDITING_STACK: 'SET_EDITING_STACK',
+};
 
 import _pick from 'lodash/pick';
+
+const getNewStack = () => ({
+  name: '',
+  desc: '',
+  icon: '',
+});
 
 const state = {
   intro: '',
@@ -39,51 +48,47 @@ const state = {
   postSortField: null,
   postSearch: '',
   currentPost: {},
-  newStack: {
-    name: '',
-    desc: '',
-    icon: '',
-  },
+  newStack: getNewStack(),
   editingStack: false,
 };
 
 const mutations = {
-  [SET_INTRO](state, payload) {
+  [EDITING_MUTATIONS.SET_INTRO](state, payload) {
     state.intro = payload;
   },
-  [SET_POST](state, payload) {
+  [EDITING_MUTATIONS.SET_POST](state, payload) {
     state.currentPost = payload;
   },
-  [SET_POSTS](state, payload) {
+  [EDITING_MUTATIONS.SET_POSTS](state, payload) {
     state.posts = payload;
   },
-  [SET_TOTAL_PAGES](state, payload) {
+  [EDITING_MUTATIONS.SET_TOTAL_PAGES](state, payload) {
     state.postTotalPages = payload;
   },
-  [SET_POST_PAGE](state, payload) {
+  [EDITING_MUTATIONS.SET_POST_PAGE](state, payload) {
     state.postPage = payload;
   },
-  [ADD_POST](state, payload) {
+  [EDITING_MUTATIONS.ADD_POST](state, payload) {
     state.posts.push(payload);
   },
-  [SET_SORT](state, { prop, order }) {
+  [EDITING_MUTATIONS.SET_SORT](state, { prop, order }) {
     state.postSortDirection = order;
     state.postSortField = prop;
   },
-  [SET_POST_SEARCH](state, payload) {
+  [EDITING_MUTATIONS.SET_POST_SEARCH](state, payload) {
     state.postSearch = payload;
   },
-  [SET_STACKS](state, payload) {
+  [EDITING_MUTATIONS.SET_STACKS](state, payload) {
     state.stacks = payload;
   },
-  [SET_STACK](state, stack) {
+  [EDITING_MUTATIONS.SET_STACK](state, stack) {
     const idx = state.stacks.findIndex(s => s._id === stack._id);
     Vue.set(state.stacks, idx, stack);
   },
-  [SET_NEW_STACK](state, payload) {
+  [EDITING_MUTATIONS.SET_NEW_STACK](state, payload) {
     state.newStack = payload;
   },
-  [SET_EDITING_STACK](state, payload) {
+  [EDITING_MUTATIONS.SET_EDITING_STACK](state, payload) {
     state.editingStack = payload;
   },
 };
@@ -91,16 +96,16 @@ const mutations = {
 const actions = {
   async getIntro({ commit }) {
     const intro = await getIntro();
-    commit(SET_INTRO, intro);
+    commit(EDITING_MUTATIONS.SET_INTRO, intro);
   },
   async saveIntro({ commit }, intro) {
     const res = await saveIntro(intro);
     if (res) Notification.success({ message: 'successfully updated intro' });
-    commit(SET_INTRO, res);
+    commit(EDITING_MUTATIONS.SET_INTRO, res);
   },
   async getPost({ commit }, postId) {
     const post = await getPost(postId);
-    commit(SET_POST, post);
+    commit(EDITING_MUTATIONS.SET_POST, post);
   },
   async getPosts({ state, commit }) {
     const optPagination = {
@@ -113,13 +118,13 @@ const actions = {
     }
     if (state.postSearch !== '') optPagination.title = state.postSearch;
     const posts = await getPosts(optPagination);
-    commit(SET_POSTS, posts.docs);
-    commit(SET_TOTAL_PAGES, posts.totalPages);
+    commit(EDITING_MUTATIONS.SET_POSTS, posts.docs);
+    commit(EDITING_MUTATIONS.SET_TOTAL_PAGES, posts.totalPages);
   },
   async createPost({ commit }, post) {
     const res = await createPost(post);
     if (res) Notification.success({ message: 'successfully made a new post' });
-    commit(ADD_POST, res);
+    commit(EDITING_MUTATIONS.ADD_POST, res);
   },
   async modifyPost({ commit }, post) {
     const newPost = _pick(post, ['_id', 'title', 'content', 'images']);
@@ -127,7 +132,7 @@ const actions = {
     return res;
   },
   async setPostPage({ commit, dispatch }, page) {
-    commit(SET_POST_PAGE, page);
+    commit(EDITING_MUTATIONS.SET_POST_PAGE, page);
     await dispatch('getPosts');
   },
   async deletePosts({ dispatch }, postIds) {
@@ -136,37 +141,32 @@ const actions = {
     await dispatch('getPosts');
   },
   async setSort({ commit, dispatch }, { prop, order }) {
-    commit('SET_SORT', { prop, order });
+    commit(EDITING_MUTATIONS.SET_SORT, { prop, order });
     await dispatch('getPosts');
   },
   async getStacks({ commit }) {
     const stacks = await getStacks();
-    commit('SET_STACKS', stacks);
+    commit(EDITING_MUTATIONS.SET_STACKS, stacks);
   },
   openStack({ commit }, stackId) {
     const newStack = state.stacks.find(s => s._id === stackId);
-    commit(SET_NEW_STACK, newStack);
-    commit(SET_EDITING_STACK, true);
+    commit(EDITING_MUTATIONS.SET_NEW_STACK, newStack);
+    commit(EDITING_MUTATIONS.SET_EDITING_STACK, true);
   },
   openNewStack({ commit }) {
-    const newStack = {
-      name: '',
-      icon: '',
-      desc: '',
-    };
-    commit(SET_NEW_STACK, newStack);
-    commit(SET_EDITING_STACK, true);
+    commit(EDITING_MUTATIONS.SET_NEW_STACK, getNewStack());
+    commit(EDITING_MUTATIONS.SET_EDITING_STACK, true);
   },
   editStackByKey({ state, commit }, obj) {
     const newStack = Object.assign(state.newStack, obj);
-    commit(SET_NEW_STACK, newStack);
+    commit(EDITING_MUTATIONS.SET_NEW_STACK, newStack);
   },
   async createStack({ commit, state, dispatch }, fileurl) {
     const newStack = Object.assign(state.newStack, { icon: fileurl });
     console.log('new stack', newStack);
     await createStack(newStack);
     await dispatch('getStacks');
-    commit(SET_EDITING_STACK, false);
+    commit(EDITING_MUTATIONS.SET_EDITING_STACK, false);
   },
   async deleteStacks({ dispatch }, stackIds) {
     const deletions = stackIds.map(stackId => deleteStack(stackId));
@@ -177,7 +177,7 @@ const actions = {
     const newStack = Object.assign(state.newStack, { icon: fileurl });
     await modifyStack(newStack);
     await dispatch('getStacks');
-    commit(SET_EDITING_STACK, false);
+    commit(EDITING_MUTATIONS.SET_EDITING_STACK, false);
   },
 };
 
